a041aa4c6505d8288ea5433ed15670a31dc571dd
Lahmar22 <lahmarzakariae22@gmail.com>
2025-12-23 23:10:04 +0100

    feat : add function reserveVisiteGuid a class visitor

--- Models/visitor.php
+++ Models/visitor.php
@@ -1,4 +1,7 @@
 <?php 
+require_once 'visitrGuid.php';
+require_once 'database.php';
+
 class Visitor extends User
 { 
     private $role;
@@ -6,6 +9,24 @@ class Visitor extends User
     {
         parent::__construct($name, $firstName, $email, $password, $statut);
         $this->role = $role;
+
+    }
+
+    public function reserveVisitGuid(VisitesGuides $visitesGuides){
+        $reserveVisite = "INSERT INTO visitesguidees (titre, dateheure, langue, capacite_max, statut, duree, prix)
+        VALUES(?, ?, ?, ?, ?, ?, ? )";
+
+        $db = Database::connect();
+        $stmt = $db->prepare($reserveVisite);
+        return $stmt->execute([
+            $visitesGuides->getTitre(),
+            $visitesGuides->getDateheure(),
+            $visitesGuides->getLangue(),
+            $visitesGuides->getCapaciteMax(),
+            $visitesGuides->getStatut(),
+            $visitesGuides->getDuree(),
+            $visitesGuides->getPrix()
+        ]);
 
     }
 
