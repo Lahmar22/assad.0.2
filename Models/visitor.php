16ddcc07ed0e58d55b8ad86aab93f46ce3a665ca
Lahmar22 <lahmarzakariae22@gmail.com>
2025-12-23 20:31:16 +0100

    feat : add class visitor oop

---
+++ Models/visitor.php
@@ -0,0 +1,15 @@
+<?php 
+class Visitor extends User
+{ 
+    private $role;
+    public function __construct($name, $firstName, $email, $password, $statut, $role)
+    {
+        parent::__construct($name, $firstName, $email, $password, $statut);
+        $this->role = $role;
+
+    }
+
+
+
+}
+?>
