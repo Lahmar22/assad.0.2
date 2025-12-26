a99c6a8cf8d612ca7e54a6b82c52860cf5516e37
Lahmar22 <lahmarzakariae22@gmail.com>
2025-12-26 08:59:26 +0100

    feat : add function remove visiteGuid in class RemoveVisitGuid

---
+++ controllers/removeVisitGuid.php
@@ -0,0 +1,32 @@
+<?php
+require_once '../Models/visiteGuid.php';
+
+class RemoveVisiteGuid
+{
+
+    public function removeVisiteGuid()
+    {
+        if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+            return;
+        }
+
+        try {
+            $id = $_POST["id_visitGuid"];
+
+            $visitesG = new VisitesGuides();
+            $visitesG->removeVisitGuid($id);
+
+            header("Location: ../Views/guid/home.php?success=1");
+            exit();
+        } catch (PDOException $e) {
+
+            echo "<pre>";
+            echo "DB ERROR : " . $e->getMessage();
+            echo "</pre>";
+            exit();
+        }
+    }
+}
+
+$remove = new RemoveVisiteGuid();
+$remove->removeVisiteGuid();
